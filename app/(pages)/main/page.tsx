6765a36a43703d8f148049dcee26f0151b7aa2d1
ViniciusGNunes <vini.gn14@gmail.com>
2026-07-14 21:05:34 -0400

    adding routing and main page

---
+++ app/(pages)/main/page.tsx
@@ -0,0 +1,10 @@
+
+import styles from './page.module.scss'
+
+export default function main(){
+    return (
+        <div className={styles.init}>
+            Welcome to the main page
+        </div>
+    )
+}
